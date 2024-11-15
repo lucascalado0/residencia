5407bbf5b294cabeb82a789cfb7ca4c72744b2b8
devlucascalado <devlucascalado@gmail.com>
2024-11-15 15:14:57 -0300

    incluindo cadastro

--- src/Components/Cadastro_user.jsx
+++ src/Components/Cadastro_user.jsx
@@ -1,13 +1,30 @@
 import React from 'react'
+import { Link } from "react-router-dom"
 
 
 function Cadastro () {
     return (
-        <div>
-            <p>aaaaaaaaa</p>
+        <div className="container">
+           <div className='item1'></div>
+            <div className='item2'>
+                <form action="" method="post" className='formulario'>
+                    <label htmlFor="name">Nome de Usuário: </label>
+                    <input type="text" placeholder='Insira seu usuario' required/>
+                    <label htmlFor="email">Email: </label>
+                    <input type="email" placeholder='Insira seu email' required/>
+                    <label htmlFor="password">Senha: </label>
+                    <input type="password" placeholder='Insira sua senha'required/>
+                    <label htmlFor="funcao">Função: </label>
+                    <select name="funcao" id="" className='select_funcao'>
+                        <option value="funcao1">Função 1</option>
+                        <option value="funcao2">Função 2</option>
+                        <option value="funcao3">Função 3</option>
+                    </select>
+                    <button type="button">CADASTRAR</button>
+                </form>
+                <Link to='/' className='link_cadastro'>Já possui login?</Link>
+            </div>
         </div>
-        
-
     )
 
 }
--- src/Components/Login.jsx
+++ src/Components/Login.jsx
@@ -1,37 +1,22 @@
 import React from 'react'
-import {Link} from "react-router-dom"
-
+import { Link } from "react-router-dom"
 
 
 function Login() {
     return (
         <div className="container">
-            <form className="login-form">
-                <div className="form-group">
-                    <label htmlFor="username">Nome de Usuário</label>
-                    <input
-                        type="text"
-                        id="username"
-                        name="username"
-                        placeholder="Digite seu usuário"
-                        required
-                    />
-                </div>
-                <div className="form-group">
-                    <label htmlFor="password">Senha</label>
-                    <input
-                        type="password"
-                        id="password"
-                        name="password"
-                        placeholder="Digite sua senha"
-                        required
-                    />
-                </div>
-                <button type="submit">Entrar</button>
-            </form>
-            <Link to={'/cadastro_usuario'}>Cadastre-se</Link>
+            <div className='item1'></div>
+            <div className='item2'>
+                <form action="" method="post" className='formulario'>
+                    <label htmlFor="name">Nome de Usuário: </label>
+                    <input type="text" placeholder='Insira seu usuario' />
+                    <label htmlFor="password">Senha: </label>
+                    <input type="password" placeholder='Insira sua senha'/>
+                    <button type="button">LOGIN</button>
+                </form>
+                <Link to='/cadastro_usuario' className='link_cadastro'>Cadastre-se</Link>
+            </div>
         </div>
-        
     )
 
 }
