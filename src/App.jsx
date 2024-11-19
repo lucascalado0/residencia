d4efb44017980c66b3e20d50b7c06f21895819e7
devlucascalado <devlucascalado@gmail.com>
2024-11-19 18:30:43 -0300

    .

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import './App.css'
 import './Components/Login.css'
 import Login from './Components/Login'
 import Cadastro from './Components/Cadastro_user'
+import Admin from './Components/admin_pagina'
 
 import {BrowserRouter, Route, Routes} from 'react-router-dom'
 
@@ -14,6 +15,7 @@ function App() {
     <Routes>
       <Route path='/' element={<Login/>}/>
       <Route path='/cadastro_usuario' element={<Cadastro/>}/>
+      <Route path='/admin_page' element={<Admin/>}/>
       <Route path='*' element={<h1>Not Found</h1>}/>
     </Routes>
     </BrowserRouter>
--- src/Components/Cadastro_user.jsx
+++ src/Components/Cadastro_user.jsx
@@ -1,13 +1,14 @@
 import React from 'react'
 import { Link } from "react-router-dom"
+import styles from './Cadastro_user.module.css'
 
 
 function Cadastro () {
     return (
-        <div className="container">
-           <div className='item1'></div>
-            <div className='item2'>
-                <form action="" method="get" className='formulario'>
+        <div className={styles.container}>
+           <div className={styles.item1}></div>
+            <div className={styles.item2}>
+                <form action="" method="get" className={styles.formulario}>
                     <label htmlFor="name">Nome de Usuário: </label>
                     <input type="text" placeholder='Insira seu usuario' required/>
                     <label htmlFor="email">Email: </label>
@@ -15,14 +16,14 @@ function Cadastro () {
                     <label htmlFor="password">Senha: </label>
                     <input type="password" placeholder='Insira sua senha'required/>
                     <label htmlFor="funcao">Função: </label>
-                    <select name="funcao" id="" className='select_funcao'>
+                    <select name="funcao" id="" className={styles.select_funcao}>
                         <option value="funcao1">FUNÇÃO 1</option>
                         <option value="funcao2">FUNÇÃO 2</option>
                         <option value="funcao3">FUNÇÃO 3</option>
                     </select>
                     <button type="submit">CADASTRAR</button>
                 </form>
-                <Link to='/' className='link_cadastro'>Já possui login?</Link>
+                <Link to='/' className={styles.link_cadastro}>Já possui login?</Link>
             </div>
         </div>
     )
