7b1c961d1dc0b0501e445635347016a319a2538c
Ruan12419 <az.88004607@gmail.com>
2024-11-25 14:43:53 -0300

    Models do BD

---
+++ backend/app/models/evidencias.py
@@ -0,0 +1,13 @@
+from datetime import datetime
+from pymongo import MongoClient
+import os
+from dotenv import load_dotenv
+import pytz
+
+load_dotenv()
+
+client = MongoClient(os.getenv("MONGO_URI"))
+db = client['thehive']
+
+# TODO
+# class Evidencias:
